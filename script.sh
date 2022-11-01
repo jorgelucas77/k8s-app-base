1e7d3567101d8fb68ce0c3a90b48559a0f8ee594
Jorge Lucas <jorgelucas77@gmail.com>
2022-11-01 19:25:48 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "A Criar as imagens"
+
+docker build -t jorgelucas/projeto-backend:1.0 backend/.
+docker build -t jorgelucas/projeto-database:1.0 database/.
+
+echo "A fazer o upload das imagens"
+
+docker push jorgelucas/projeto-backend:1.0 backend/.
+docker push jorgelucas/projeto-database:1.0 database/.
+
+echo "Criar serviços no cluster de Kubernetes"
+
+kubectl apply -f ./services.yml
+
+echo "Criar os deployments no cluster de Kubernetes"
+
+kubectl apply -f ./deployment.yml
